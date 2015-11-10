9bb601d3607673e78c06d94a4c1eeb0254325295
Christian Fei <crifei93@gmail.com>
2015-11-10 22:26:49 +0100

    migrating ecto at infrastructure startup

--- api_v2/config/config.exs
+++ api_v2/config/config.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :api_v2, http_port: 6000, api_url: "http://pomodoro-api:6000"
 config :api_v2, ApiV2.Repo,
   adapter: Ecto.Adapters.Postgres,
-  hostname: "postgres",
+  hostname: "pomodoro-api-db_v2",
   database: "api_v2",
   username: "postgres",
   password: "postgres"
--- api_v2/test/api_v2_test.exs
+++ api_v2/test/api_v2_test.exs
@@ -1,8 +1,3 @@
 defmodule ApiV2Test do
   use ExUnit.Case
-  doctest ApiV2
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
