f71ae7a75500ae309c6fc3be105e29f6683dd478
Christian Fei <crifei93@gmail.com>
2015-11-11 22:31:14 +0100

    wip auth test

---
+++ api_v2/test/api_v2/router_test.exs
@@ -0,0 +1,14 @@
+defmodule ApiV2.Router.Test do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  @tag :skip
+  test "authenticates requests" do
+    conn = conn(:get, "/api/settings")
+             |> put_req_header("cookie", "invalid")
+             |> ApiV2.Router.call([])
+
+    assert conn.status == 401
+  end
+
+end
