81f9143d10e2719011fc1421449725bc7e13bb15
Christian Fei <crifei93@gmail.com>
2015-11-12 00:09:58 +0100

    wip Authorizer plug

--- api_v2/config/config.exs
+++ api_v2/config/config.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
-config :api_v2, http_port: 6000, api_url: "http://pomodoro-api:6000"
+config :api_v2,
+  http_port: 6000,
+  authorizer_url: "http://pomodoro-api:6000"
 
 import_config "#{Mix.env}.exs"
--- api_v2/lib/api_v2/router.ex
+++ api_v2/lib/api_v2/router.ex
@@ -1,17 +1,19 @@
 defmodule ApiV2.Router do
   use Plug.Router
 
-  alias ApiV2.FallbackProxy
-
-  @api_url Application.get_env(:api_v2, :api_url)
+  alias ApiV2.Authorizer
 
   plug Plug.Logger
   if Mix.env == :dev, do: use Plug.Debugger
 
+  plug Authorizer
   plug :match
   plug :dispatch
 
   get "/api/tasks" do
+    IO.puts "--> authorized.."
+
+    # put_resp_header(conn, "content-type", "application/json")
     send_resp(conn, 200, Poison.encode!([]))
   end
 end
