65717293ecf2c0b0386327f15cfa4b147b451b48
Christian Fei <crifei93@gmail.com>
2015-11-12 22:45:44 +0100

    improved names and formatting

--- api_v2/lib/api_v2/authorizer_plug.ex
+++ api_v2/lib/api_v2/authorizer_plug.ex
@@ -10,9 +10,13 @@ defmodule ApiV2.Authorizer.Plug do
     authorizer = Keyword.get(opts, :authorizer, ApiV2.Authorizer)
     get_req_header(conn, "cookie")
     |> authorizer.authorize
-    |> handle(conn)
+    |> handle_authorization(conn)
   end
 
-  defp handle(true, conn), do: conn
-  defp handle(false, conn), do: conn |> send_resp( 401, "Unauthorized") |> halt
+  defp handle_authorization(true, conn), do: conn
+  defp handle_authorization(false, conn)do
+    conn
+    |> send_resp(401, "Unauthorized")
+    |> halt
+  end
 end
