10add1df61c67a9629d0cad4d7461e811e05016e
Christian Fei <crifei93@gmail.com>
2015-11-15 16:35:12 +0100

    added add todo reducer test

--- app_v2/src/reducers/todos.test.js
+++ app_v2/src/reducers/todos.test.js
@@ -1,6 +1,12 @@
 import todos from './todos'
+import {addTodo} from '../actions'
 describe('todos reducer', () => {
+  const todo = {completed:false,text:'i am todo'}
   it('adds todo', () => {
-
+    let state = []
+    state = todos(state, addTodo(todo))
+    expect( state ).to.deep.eql( [todo] )
+    state = todos(state, addTodo(todo))
+    expect( state ).to.deep.eql( [todo, todo] )
   })
 })
