a17459f07e9c0434848574831db5b8617bcff610
Christian Fei <crifei93@gmail.com>
2015-11-15 16:37:06 +0100

    added delete todo reducer

--- app_v2/src/reducers/todos.test.js
+++ app_v2/src/reducers/todos.test.js
@@ -1,12 +1,20 @@
 import todos from './todos'
-import {addTodo} from '../actions'
+import {addTodo, deleteTodo} from '../actions'
 describe('todos reducer', () => {
   const todo = {completed:false,text:'i am todo'}
+  const todo2 = {completed:true,text:'i am todo2'}
   it('adds todo', () => {
     let state = []
     state = todos(state, addTodo(todo))
     expect( state ).to.deep.eql( [todo] )
-    state = todos(state, addTodo(todo))
-    expect( state ).to.deep.eql( [todo, todo] )
+    state = todos(state, addTodo(todo2))
+    expect( state ).to.deep.eql( [todo, todo2] )
+  })
+  it('deletes todo', () => {
+    let state = [todo, todo2]
+    state = todos(state, deleteTodo(todo))
+    expect( state ).to.deep.eql( [todo2] )
+    state = todos(state, deleteTodo(todo2))
+    expect( state ).to.deep.eql( [] )
   })
 })
