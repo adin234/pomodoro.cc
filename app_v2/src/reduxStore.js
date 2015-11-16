d27bf52f6759b8fcc240a44da6fc1e17947dbf05
Christian Fei <crifei93@gmail.com>
2015-11-16 18:55:07 +0100

    removed obsolete code

--- app_v2/src/reduxStore.js
+++ app_v2/src/reduxStore.js
@@ -5,7 +5,6 @@ import settings from './reducers/settings'
 import user from './reducers/user'
 import {applyMiddleware, combineReducers, createStore, compose} from 'redux'
 import thunk from 'redux-thunk'
-import {Provider} from 'react-redux'
 import createLogger from 'redux-logger'
 import persistState from 'redux-localstorage'
 
