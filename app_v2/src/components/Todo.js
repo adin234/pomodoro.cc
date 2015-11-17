ef4ff62dd8095d6377f14eddde44aa901810da13
Christian Fei <crifei93@gmail.com>
2015-11-17 22:34:03 +0100

    refactoring todo

--- app_v2/src/components/Todo.js
+++ app_v2/src/components/Todo.js
@@ -2,23 +2,16 @@ require('./Todo.styl')
 import React, {Component, PropTypes} from 'react'
 
 export default class Todo extends Component {
-  deleteTodo (todo) {
-    const {actions} = this.props
-    actions.deleteTodo(todo)
-  }
-
-  toggleCompleteTodo (todo) {
-    const {actions} = this.props
-    actions.toggleCompleteTodo(todo)
-  }
-
   render () {
-    const {todo} = this.props
+    const {todo, actions} = this.props
     const className = "todo " + (todo.completed?"completed":"")
     return  <li className={className}>
-              <input className="toggle" type="checkbox" onClick={()=>this.toggleCompleteTodo(todo)}/>
+              <input className="toggle" type="checkbox"
+                onClick={()=>actions.toggleCompleteTodo(todo)}/>
               <label>{todo.text}</label>
-              <button onClick={()=>this.deleteTodo(todo)} className="destroy"></button>
+              <button
+                className="destroy"
+                onClick={()=>actions.deleteTodo(todo)}></button>
             </li>
   }
 }
