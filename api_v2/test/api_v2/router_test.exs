f2a495a6f2fb0594ad8702a942b994b0ee14622c
Christian Fei <crifei93@gmail.com>
2015-11-21 17:09:33 +0100

    added integration router test

--- api_v2/test/api_v2/router_test.exs
+++ api_v2/test/api_v2/router_test.exs
@@ -2,7 +2,7 @@ defmodule ApiV2.Router.Test do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  @tag :skip
+  @tag :noskip
   test "authenticates requests" do
     conn = conn(:get, "/api/settings")
              |> put_req_header("cookie", "invalid")
