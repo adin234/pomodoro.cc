b8a4b816900188696eebd6fe4cb31c3f379aacd1
Christian Fei <crifei93@gmail.com>
2015-11-27 00:53:22 +0100

    updated welcome text

--- app_v2/src/components/WelcomeBar.js
+++ app_v2/src/components/WelcomeBar.js
@@ -13,7 +13,7 @@ export default class WelcomeBar extends Component {
                 <i onClick={() => actions.acknowledgeWelcome()} style={{"float":"right"}} className="icon ion-close-round"></i>
                 <h3>Welcome to pomodoro.cc!</h3>
                 <p>We can help you to plan your activities for the day, and get things done!</p>
-                <p>Pomodoro.cc is a tool that leverages the concepts of the <a href="http://pomodorotechnique.com/" target="_blank">Pomodoro Technique</a> to help you to keep your focus, get things done with frequent, mind-refreshing breaks.</p>
+                <p>Pomodoro.cc is a tool that leverages the concepts of the <a href="http://pomodorotechnique.com/" target="_blank">Pomodoro Technique</a> to help you to keep your focus with mind-refreshing breaks.</p>
                 <p>By keeping track of your activities, we can aggregate insightful statistics to help you to become more productive.</p>
               </div>
             </div>
