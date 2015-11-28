1ec6c7327620089aaa01db80ce174f5b9056f8b3
Christian Fei <crifei93@gmail.com>
2015-11-28 20:38:25 +0100

    refactoring tests

--- api_v2/test/api_v2/repo_test.exs
+++ api_v2/test/api_v2/repo_test.exs
@@ -14,12 +14,29 @@ defmodule ApiV2.Repo.Test do
     :ok
   end
 
-  test "#tasks_for" do
-    assert Repo.tasks_for(@user_id) == []
-  end
-
   test "#create_pomodoro_task_for" do
     {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
+  end
+
+  test "#tasks_for" do
+    assert Repo.tasks_for(@user_id) == []
+    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
     assert Repo.tasks_for(@user_id) == [pomodoro_task]
+  end
+
+  test "#task_for" do
+    assert Repo.task_for(@user_id, 0) == nil
+    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
+    assert Repo.task_for(@user_id, pomodoro_task.id) == pomodoro_task
+  end
+
+
+  @tag :skip
+  test "#update_pomodoro_task_for" do
+    updated_text = "updated text"
+    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
+    updated_pomodoro_task = PomodoroTask.changeset(pomodoro_task, %{text: "updated text"})
+    Repo.update_pomodoro_task_for(@user_id, updated_pomodoro_task)
+    assert Repo.task_for(@user_id, pomodoro_task.id).text == updated_text
   end
 end
