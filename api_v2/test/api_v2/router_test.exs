7e4adb6787b0892d97f0ec7f9026665e6816ba22
Christian Fei <crifei93@gmail.com>
2015-11-28 13:07:47 +0100

    removed obsolete code

--- api_v2/test/api_v2/router_test.exs
+++ api_v2/test/api_v2/router_test.exs
@@ -3,20 +3,10 @@ defmodule ApiV2.Router.Test do
   use Plug.Test
 
   test "authenticates requests" do
-    conn = conn(:get, "/api/settings")
+    conn = conn(:get, "/api/")
              |> put_req_header("cookie", "invalid")
              |> ApiV2.Router.call([])
 
     assert conn.status == 401
   end
-
-  @tag :skip
-  test "gets tasks for user" do
-    conn = conn(:get, "/api/tasks")
-             |> put_req_header("cookie", "invalid")
-             |> ApiV2.Router.call([])
-
-    assert conn.status == 200
-  end
-
 end
