96e10f5cc548230258925c71de0cd17b75fc2508
Christian Fei <crifei93@gmail.com>
2015-11-28 20:46:25 +0100

    refactoring tests

--- api_v2/test/api_v2/repo_test.exs
+++ api_v2/test/api_v2/repo_test.exs
@@ -7,6 +7,7 @@ defmodule ApiV2.Repo.Test do
 
   @user_id 1
   @pomodoro_task %PomodoroTask{text: "test pomodoro_task"}
+  @updated_text "Rephrasing the task text"
 
   setup do
     Repo.delete_all(UserPomodoroTask)
@@ -15,28 +16,30 @@ defmodule ApiV2.Repo.Test do
   end
 
   test "#create_pomodoro_task_for" do
-    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
+    {:ok, pomodoro_task} = create_pomodoro
   end
 
   test "#tasks_for" do
     assert Repo.tasks_for(@user_id) == []
-    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
+    {:ok, pomodoro_task} = create_pomodoro
     assert Repo.tasks_for(@user_id) == [pomodoro_task]
   end
 
   test "#task_for" do
     assert Repo.task_for(@user_id, 0) == nil
-    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
+    {:ok, pomodoro_task} = create_pomodoro
     assert Repo.task_for(@user_id, pomodoro_task.id) == pomodoro_task
   end
 
-
-  @tag :skip
   test "#update_pomodoro_task_for" do
-    updated_text = "updated text"
-    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
-    updated_pomodoro_task = PomodoroTask.changeset(pomodoro_task, %{text: "updated text"})
+    {:ok, pomodoro_task} = create_pomodoro
+    updated_pomodoro_task = PomodoroTask.changeset(pomodoro_task, %{text: @updated_text})
     Repo.update_pomodoro_task_for(@user_id, updated_pomodoro_task)
-    assert Repo.task_for(@user_id, pomodoro_task.id).text == updated_text
+    updated_pomodoro_task_in_db = Repo.task_for(@user_id, pomodoro_task.id)
+    assert updated_pomodoro_task_in_db.text == @updated_text
+  end
+
+  defp create_pomodoro do
+    Repo.create_pomodoro_task_for(@user_id, @pomodoro_task)
   end
 end
