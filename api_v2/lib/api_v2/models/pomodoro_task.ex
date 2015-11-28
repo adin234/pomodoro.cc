fecddf3c3d09cedaca8fd1a6f3a2954d9d0d9511
Christian Fei <crifei93@gmail.com>
2015-11-28 15:20:41 +0100

    added and getting pomodoro_tasks

--- api_v2/lib/api_v2/models/pomodoro_task.ex
+++ api_v2/lib/api_v2/models/pomodoro_task.ex
@@ -1,6 +1,7 @@
 defmodule ApiV2.Models.PomodoroTask do
   use Ecto.Model
 
+  @derive {Poison.Encoder, only: [:id, :text, :completed, :deleted, :inserted_at, :updated_at]}
   schema "pomodoro_task" do
     field :text,         :string
     field :completed,    :boolean, default: false
--- api_v2/lib/api_v2/router.ex
+++ api_v2/lib/api_v2/router.ex
@@ -1,18 +1,32 @@
 defmodule ApiV2.Router do
   use Plug.Router
 
+  # todo: alias ApiV2..
+  alias ApiV2.Repo
+  alias ApiV2.Models.PomodoroTask
+
   plug Plug.Logger
   if Mix.env == :dev, do: use Plug.Debugger
 
   plug ApiV2.Authorizer.Plug
+  plug Plug.Parsers, parsers: [:json],
+                     json_decoder: Poison
   plug :match
   plug :dispatch
 
   get "/api/tasks" do
     user = conn.assigns[:user]
     user_id = Dict.get(user, "id")
-    pomodoro_tasks = ApiV2.Repo.tasks_for(user_id)
-    IO.inspect pomodoro_tasks
+    pomodoro_tasks = Repo.tasks_for(user_id)
     send_resp(conn, 200, Poison.encode!(pomodoro_tasks))
+  end
+
+  post "/api/tasks" do
+    user = conn.assigns[:user]
+    user_id = Dict.get(user, "id")
+    pomodoro_task_body = conn.params
+    changeset = Ecto.Changeset.cast(%PomodoroTask{}, pomodoro_task_body, ~w(text), ~w())
+    {:ok, pomodoro_task} = Repo.create_pomodoro_task_for(user_id, changeset)
+    send_resp(conn, 201, Poison.encode!(pomodoro_task))
   end
 end
