a2a889ce49b3c6d216ee0d73bfb6b7bb036a0e48
Christian Fei <crifei93@gmail.com>
2015-12-16 08:14:33 +0100

    using underscore for completed sorting

--- app_v2/src/reducers/todos.js
+++ app_v2/src/reducers/todos.js
@@ -6,21 +6,22 @@ import {
   UPDATE_TODO_SUCCESS
 } from '../actions/todos'
 
+import _ from 'underscore'
+
 export default function todos(state:TodoState=[], action:Action):TodoState {
-  // debugger
   switch(action.type){
   case GET_TODO_SUCCESS: {
-    return action.payload.todos
+    return sortCompleted(action.payload.todos)
   }
   case ADD_TODO_SUCCESS: {
     let newTodo = action.payload
     if( !newTodo.id ){
-      newTodo.id = state.reduce((acc, v) => (acc > v ? acc : v), 0)
+      newTodo.id = state.reduce(max, 0)
     }
-    return [
+    return sortCompleted([
       ...state,
       newTodo
-    ]
+    ])
   }
   case DELETE_TODO_SUCCESS: {
     return state.filter((todo) => {
@@ -28,16 +29,29 @@ export default function todos(state:TodoState=[], action:Action):TodoState {
     })
   }
   case UPDATE_TODO_SUCCESS: {
-    return state.map((todo) => {
+    return sortCompleted(state.map((todo) => {
       return (todo.id !== action.payload.id)
               ? todo
               : action.payload
-    })
+    }))
   }
   }
   return state
 }
 
+const max = (acc, curr) => (acc > curr.id ? acc : curr.id+1)
+
 const sortCompleted = (todos) => {
-  return todos.sort((t) => t.completed)
+  return _.sortBy(todos, 'completed')
+  return todos.sort((t1, t2) => {
+    let sorting = (t1.completed===t2.completed) ? 1 : -1
+    sorting -= (t2.id - t2.id)
+
+    return sorting
+    if( t1.completed === t2.completed ) {
+      return t1.id - t2.id
+    }
+    return t1.id - t2.id
+  })
 }
+
