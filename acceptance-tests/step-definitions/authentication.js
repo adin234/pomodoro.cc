6a67b361e358d4a07d4acf652740a260ed85d567
Christian Fei <crifei93@gmail.com>
2016-01-28 23:57:24 +0100

    improved authentication e2e test

--- acceptance-tests/step-definitions/authentication.js
+++ acceptance-tests/step-definitions/authentication.js
@@ -1,7 +1,10 @@
 module.exports = function() {
   this.Then(/^I login$/, function() {
-    this.url('https://pomodoro.dev/auth/fake')
+    this
     .waitForElementVisible('body', 5000)
+    .click('a[href="/login"]')
+    .assert.containsText('.main-content', 'Howdy!')
+    .url('https://pomodoro.dev/auth/fake')
   })
   this.Then(/^I should be logged in$/, function() {
     this.assert.visible('.profile')
