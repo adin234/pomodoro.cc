d0a442652fedc39f8410df8d4fa707fde10a89b5
Christian Fei <crifei93@gmail.com>
2016-02-08 23:37:06 +0100

    added pause in acceptance test between scenarios

--- acceptance-tests/step-definitions/pomodoro-timer.js
+++ acceptance-tests/step-definitions/pomodoro-timer.js
@@ -2,12 +2,15 @@ module.exports = function() {
   this.Then(/^I interact with the timer$/, function() {
     this
     .waitForElementVisible('body', 5000)
+    .assert.containsText('.main-content', '00:00')
     .click('.timer-buttons-container #minutes-25')
-    .assert.containsText('.main-content', '25:00')
+    .pause(1000)
     .expect.element('.timer').text.to.match(/2(5|4)\:\d\d/)
     .click('.timer-buttons-container #minutes-5')
+    .pause(1000)
     .expect.element('.timer').text.to.match(/0(5|4)\:\d\d/)
     .click('.timer-buttons-container #minutes-15')
+    .pause(1000)
     .expect.element('.timer').text.to.match(/1(5|4)\:\d\d/)
   })
 }
