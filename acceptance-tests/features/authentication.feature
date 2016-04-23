8dc0bcc269d6c78cb70dc31ac59e970b22e832d9
Christian Fei <crifei93@gmail.com>
2016-04-23 13:05:29 +0200

    improve acceptance tests

--- acceptance-tests/features/authentication.feature
+++ acceptance-tests/features/authentication.feature
@@ -1,5 +1,7 @@
 Feature: Authentication
 Scenario: As a user I can log in
   Given I open pomodoro.dev
+  When I go to the login page
+  Then I should see the login page
   When I login
   Then I should be logged in
--- acceptance-tests/step-definitions/authentication.js
+++ acceptance-tests/step-definitions/authentication.js
@@ -1,9 +1,18 @@
 module.exports = function() {
-  this.When(/^I login$/, function() {
+  this.When(/^I go to the login page$/, function() {
     this
     .waitForElementVisible('body', 5000)
     .click('a[href="/login"]')
     .assert.containsText('.main-content', 'Howdy!')
+  })
+  this.Then(/^I should see the login page$/, function() {
+    this
+    .waitForElementVisible('body', 5000)
+    .assert.containsText('.main-content', 'Howdy!')
+  })
+  this.Then(/^I login$/, function() {
+    this
+    .waitForElementVisible('body', 5000)
     .url('https://pomodoro.dev/auth/fake')
   })
   this.Then(/^I should be logged in$/, function() {
