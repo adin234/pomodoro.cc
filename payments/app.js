72e60f56bd4e0dd224a2e2bb2ee809eda005c8c9
Christian Fei <crifei93@gmail.com>
2017-10-15 21:22:54 +0200

    extract functions

--- payments/app.js
+++ payments/app.js
@@ -9,22 +9,30 @@ const server = createServer(PORT)
 server.post('/subscription', async (req, res) => {
   console.log('POST /subscription')
   console.log('  req.body', req.body)
-  const customer = await stripe.customers.create({
-    email: req.body.stripeEmail,
-    source: req.body.stripeToken
-  })
+  const customer = await createCustomer(req.body)
   console.log('-- customer.create', customer)
-  const plans = await stripe.plans.list({limit: 5})
-  console.log('-- plans.list', plans)
-  const subscription = await stripe.subscriptions.create({
-    customer: customer.id,
-    items: [{
-      plan: 'pomodoro.cc monthly'
-    }]
-  })
+  const subscription = await createSubscription(customer.id)
   console.log('-- subscriptions.create', subscription)
   res.send(JSON.stringify({customer, subscription}))
 })
 
 server.listen(PORT)
 console.log('listening on %s', PORT)
+
+// async function listPlans () {
+//   const plans = await stripe.plans.list({limit: 5})
+//   console.log('-- plans.list', plans)
+// }
+
+async function createCustomer ({ stripeEmail: email, stripeToken: source }) {
+  return stripe.customers.create({ email, source })
+}
+
+async function createSubscription (customerId, plan = 'pomodoro.cc monthly') {
+  return stripe.subscriptions.create({
+    customer: customerId,
+    items: [{
+      plan
+    }]
+  })
+}
