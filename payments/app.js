dd1e898fc2b0b3cec336de595f7ce088aefdf910
Christian Fei <crifei93@gmail.com>
2017-10-15 20:24:42 +0200

    refactor to await

--- payments/app.js
+++ payments/app.js
@@ -6,37 +6,24 @@ const createServer = require('./create-server')
 const { PORT = 3000 } = process.env
 
 const server = createServer(PORT)
-server.post('/subscription', (req, res) => {
+server.post('/subscription', async (req, res) => {
   console.log('POST /subscription')
-  console.log('  body', req.body)
-  stripe.customers.create({
+  console.log('  req.body', req.body)
+  const customer = await stripe.customers.create({
     email: req.body.stripeEmail,
     source: req.body.stripeToken
-  }, (err, customer) => {
-    console.log('customer.create', err, JSON.stringify(customer))
-    if (err) {
-      res.send(500, err)
-      console.error(err)
-    } else {
-      stripe.plans.list({limit: 5}, (err, plans) => {
-        console.log('-- plans', err, plans)
-      })
-      stripe.subscriptions.create({
-        customer: customer.id,
-        items: [{
-          plan: 'pomodoro.cc monthly'
-        }]
-      }, (err, subscription) => {
-        console.log('-- subscriptions.create', err, subscription)
-        if (err) {
-          console.error(err)
-          res.send(500, err)
-        } else {
-          res.send(JSON.stringify({customer, subscription}))
-        }
-      })
-    }
   })
+  console.log('-- customer.create', customer)
+  const plans = await stripe.plans.list({limit: 5})
+  console.log('-- plans.list', plans)
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{
+      plan: 'pomodoro.cc monthly'
+    }]
+  })
+  console.log('-- subscriptions.create', subscription)
+  res.send(JSON.stringify({customer, subscription}))
 })
 
 server.listen(PORT)
